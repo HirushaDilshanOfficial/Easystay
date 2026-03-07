0156db31959382f30aef200a016685b7281ee9c5
Omindu Ranasinghe <Ominduranasinghe@gmail.com>
2026-03-07 13:38:46 +0530

    Revert "Merge branch 'Merge/Advertise+UserM' into feature/omindu/advertising"
    This reverts commit 663682aa13f9afeea3df054d142442317cdbf120, reversing
    changes made to 66fb2c33d64f258e83c949eb1d176ae1a24f5153.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './utils/theme';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import AdminDashboard from './pages/AdminDashboard';
-import BoardingOwnerDashboard from './pages/BoardingOwnerDashboard';
-import Dashboard from './pages/Dashboard';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
-import ProtectedRoute from './components/ProtectedRoute';
+import "./App.css";
+import Home from "./Components/Advertisements";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <ProtectedRoute.RoleBased
-                  admin={<AdminDashboard />}
-                  owner={<BoardingOwnerDashboard />}
-                  fallback={<Dashboard />}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <div>
+
+      <Home />
+    </div>
   );
 }
 

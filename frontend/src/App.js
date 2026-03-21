df367c72fca4c5353abaa6c3053657df0159dc6c
Omindu Ranasinghe <Ominduranasinghe@gmail.com>
2026-03-21 18:53:25 +0530

    Update package dependencies and refactor App component for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-import Home from "./Components/Advertisements";
+import { Routes, Route } from "react-router-dom";
+import BoardingListingDashboard from "./Components/BoardingListingDashboard";
+import Advertisements from "./Components/Advertisements";
+import AdminDashboard from "./Components/AdminDashboard"; 
 
 function App() {
   return (
     <div>
-
-      <Home />
+      <Routes>
+        <Route path="/" element={<Advertisements/>} />
+        <Route path="/admin" element={<AdminDashboard/>} />
+        <Route path="/owner" element={<BoardingListingDashboard/>} />   
+      </Routes>
     </div>
   );
 }
